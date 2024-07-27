7b68180bb9496a4455a39d47e3c74225fdddc75e
tunetyme <tunetyme89@gmail.com>
2024-07-27 02:21:54 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ setup(
     name='BlackjackCardCounter',
     version='1.0',
     packages=find_packages(),
+    package_data={'': ['*.py']},  # Include all .py files
     install_requires=[
         'PyQt5==5.15.7',
     ],
     entry_points={
         'console_scripts': [
-            'blackjack-counter=main:main',
+            'blackjack-counter=BlackjackCardCounter.main:main',
         ],
     },
 )
